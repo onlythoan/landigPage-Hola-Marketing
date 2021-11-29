c6c654df53bc4e61fb15579bb5b412907a595d27
Germán Martínez <90202277+onlythoan@users.noreply.github.com>
2021-11-29 02:24:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+/* function imgSlider(anything){
+    document.querySelector('.modelo').src = anything;
+} */
