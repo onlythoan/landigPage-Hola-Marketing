480fdb3f068b08f40f983df84bc4e9ba54870afd
onlythoan <germvn86@hotmail.com>
2021-12-17 18:26:35 -0300

    responsive

--- app.js
+++ app.js
@@ -8,18 +8,3 @@ menu.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
-
-
-const sliderCell = document.querySelectorAll('.sliderCell');
-const menuSlide = document.querySelectorAll('.slide');
-
-function SetActive(i){
-    for (slide of sliderCell)
-    sliderCell[i].classList.add('active')
-}
-
-for (let j = 0; j<dots.length; j++){
-    menuSlide [j].addEventListener('click', function(){
-     SetActive(j)   
-    })
-}
